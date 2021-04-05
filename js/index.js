184b47b3f4ef18d5de2dd7c49b4b0e58516e91a7
Tania Tatula <tgrogol@gmail.com>
2021-04-05 22:37:59 +0300

    усуває помилку в кінці відліку

--- js/index.js
+++ js/index.js
@@ -1,24 +1,26 @@
 class CountdownTimer {
   constructor({ selector, targetDate }) {
     this.intervalId = null;
-
-    this.selector = selector;
+    this.selector = selector
     this.targetDate = targetDate;
+
   }
 
-  start() {
+  init() {
     const startTime = this.targetDate;
     this.intervalId = setInterval(() => {
       const currentTime = Date.now();
       const deltaTime = startTime - currentTime;
+      if (deltaTime <= 0) {
 
+        return clearInterval(this.intervalId);
+      }
       const time = this.getTimeComponents(deltaTime);
-      this.updateClockface(time);
+      this.updateClockface(time, this.selector);
     }, 1000);
   }
 
   getTimeComponents(time) {
-    if (time >= 0) {
       const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
       const hours = this.pad(
         Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
@@ -28,25 +30,24 @@ class CountdownTimer {
       );
       const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
       return { days, hours, mins, secs };
-    }
-    return clearInterval(this.intervalId);
   }
 
   pad(value) {
     return String(value).padStart(2, "0");
   }
 
-  updateClockface({ days, hours, mins, secs }) {
-    document.querySelector('[data-value="days"]').textContent = `${days}`;
-    document.querySelector('[data-value="hours"]').textContent = `${hours}`;
-    document.querySelector('[data-value="mins"]').textContent = `${mins}`;
-    document.querySelector('[data-value="secs"]').textContent = `${secs}`;
+  updateClockface({ days, hours, mins, secs }, selector) {
+    const timerSelector = document.querySelector(`${selector}`)
+    timerSelector.querySelector('[data-value="days"]').textContent = `${days}`;
+    timerSelector.querySelector('[data-value="hours"]').textContent = `${hours}`;
+    timerSelector.querySelector('[data-value="mins"]').textContent = `${mins}`;
+    timerSelector.querySelector('[data-value="secs"]').textContent = `${secs}`;
   }
 }
 
 const timer = new CountdownTimer({
   selector: "#timer-1",
-  targetDate: new Date("Jul 17, 2022"),
+  targetDate: new Date("Apr 05 2021 22:36:40"),
 });
 
-timer.start();
+timer.init();
